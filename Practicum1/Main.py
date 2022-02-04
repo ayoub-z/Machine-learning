a9de081b962d47b9bc64c3e6fd378c8a1487a319
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2022-02-04 11:52:58 +0100

    Main class to test Perceptron

---
+++ Practicum1/Main.py
@@ -0,0 +1,77 @@
+from Perceptron import Perceptron
+
+combinations1 = [[0], [1]]
+combinations2 = [[0, 0], [0, 1], [1, 0], [1, 1]]
+combinations3 = [[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 1, 1],
+				 [1, 0, 0], [1, 0, 1], [1, 1, 0], [1, 1, 1]]
+class main:
+
+	# AND-gate
+	print("\n\n-----AND-GATE-----\n")
+	perceptron1 = Perceptron()
+	
+	perceptron1.set_weight('X1', 0.5, 'O')
+	perceptron1.set_weight('X2', 0.5, 'O')
+	
+	perceptron1.set_bias(1.0)
+
+	for i in combinations2:
+		perceptron1.activate(i)
+
+
+
+	# OR-gate
+	print("\n\n-----OR-GATE-----\n")
+	perceptron2 = Perceptron()
+	
+	perceptron2.set_weight('X1', 0.5, 'O')
+	perceptron2.set_weight('X2', 0.5, 'O')
+	
+	perceptron2.set_bias(0.5)
+
+	for i in combinations2:
+		perceptron2.activate(i)
+
+
+
+	# INVERT-gate
+	print("\n\n-----INVERT-GATE-----\n")
+	perceptron3 = Perceptron()
+	
+	perceptron3.set_weight('X1', -1.0, 'O')
+	
+	perceptron3.set_bias(-0.5)
+
+	for i in combinations1:
+		perceptron3.activate(i)
+
+
+
+	# NOR-gate
+	print("\n\n-----NOR-GATE-----\n")
+	perceptron4 = Perceptron()
+	
+	perceptron4.set_weight('X1', -1.0, 'O')
+	perceptron4.set_weight('X2', -1.0, 'O')
+	perceptron4.set_weight('X3', -1.0, 'O')
+	
+	perceptron4.set_bias(0)
+
+	for i in combinations3:
+		perceptron4.activate(i)			
+
+
+
+	# PARTY-PERCEPTRON
+	print("\n\n-----PARTY-PERCEPTRON-----\n")
+	perceptron5 = Perceptron()
+	
+	perceptron5.set_weight('X1', 0.6, 'O')
+	perceptron5.set_weight('X2', 0.3, 'O')
+	perceptron5.set_weight('X3', 0.2, 'O')
+	
+	perceptron5.set_bias(0.4)
+
+	for i in combinations3:
+		perceptron5.activate(i)	
+
