832c7ea19ff707a046d7ae49fe6c962c3c952f44
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2022-02-04 15:14:43 +0100

    More comments and added parameter types

--- Practicum1/Main.py
+++ Practicum1/Main.py
@@ -8,13 +8,18 @@ class main:
 
 	# AND-gate
 	print("\n\n-----AND-GATE-----\n")
+	# Initialize the Perceptron
 	perceptron1 = Perceptron()
-	
+
+	# Here we are linking neuron 'X1' to neuron 'O'(Output)
+	# And again with 'X2' to neuron 'O'
 	perceptron1.set_weight('X1', 0.5, 'O')
 	perceptron1.set_weight('X2', 0.5, 'O')
-	
+
+	# Set the bias(threshold)
 	perceptron1.set_bias(1.0)
 
+	# And finally activate the perceptron for every possible combination
 	for i in combinations2:
 		perceptron1.activate(i)
 
@@ -23,10 +28,10 @@ class main:
 	# OR-gate
 	print("\n\n-----OR-GATE-----\n")
 	perceptron2 = Perceptron()
-	
+
 	perceptron2.set_weight('X1', 0.5, 'O')
 	perceptron2.set_weight('X2', 0.5, 'O')
-	
+
 	perceptron2.set_bias(0.5)
 
 	for i in combinations2:
@@ -37,9 +42,9 @@ class main:
 	# INVERT-gate
 	print("\n\n-----INVERT-GATE-----\n")
 	perceptron3 = Perceptron()
-	
+
 	perceptron3.set_weight('X1', -1.0, 'O')
-	
+
 	perceptron3.set_bias(-0.5)
 
 	for i in combinations1:
@@ -54,7 +59,7 @@ class main:
 	perceptron4.set_weight('X1', -1.0, 'O')
 	perceptron4.set_weight('X2', -1.0, 'O')
 	perceptron4.set_weight('X3', -1.0, 'O')
-	
+
 	perceptron4.set_bias(0)
 
 	for i in combinations3:
@@ -65,13 +70,12 @@ class main:
 	# PARTY-PERCEPTRON
 	print("\n\n-----PARTY-PERCEPTRON-----\n")
 	perceptron5 = Perceptron()
-	
+
 	perceptron5.set_weight('X1', 0.6, 'O')
 	perceptron5.set_weight('X2', 0.3, 'O')
 	perceptron5.set_weight('X3', 0.2, 'O')
-	
+
 	perceptron5.set_bias(0.4)
 
 	for i in combinations3:
 		perceptron5.activate(i)	
-
--- Practicum1/Perceptron.py
+++ Practicum1/Perceptron.py
@@ -4,10 +4,10 @@ class Perceptron:
 		self.weights = {}
 		self.bias = None
 
-	def set_weight(self, x, n, x2):
+	def set_weight(self, x: str, n: float, x2: str):
 		"""
 		x = initial neuron.
-		n = the weight.
+		n = the weight linking both neurons.
 		x2 = neuron the weight will be linked to.
 
 		Sets the weight in the form of a nested dict. Creates a dict with the 
@@ -18,14 +18,14 @@ class Perceptron:
 		weight = {}
 		weight[x2] = n
 		self.weights[x] = weight
-		
-	def set_bias(self, n):
+
+	def set_bias(self, n: float):
 		"""
 		Sets bias.
 		"""
 		self.bias = n
 
-	def activate(self, input):
+	def activate(self, input: list):
 		"""
 		Activation function. 
 		Takes a binary input, such as [0, 1] and matches them with their corresponding 
@@ -35,6 +35,7 @@ class Perceptron:
 		sum = 0
 		for counter, i in enumerate(input):
 			if i == 1:
+				# Extracts the corresponding weight.
 				# For example, it turns the dict {'X1':{'O':0.5}} into 
 				# [{'O':0.5}][counter] and then further into [0.5][0]
 				weight = list(list(self.weights.values())[counter].values())[0]
@@ -42,7 +43,7 @@ class Perceptron:
 
 		self.__str__(sum, input)
 
-	def __str__(self, sum, input):
+	def __str__(self, sum: float, input: list):
 		"""
 		Prints the input combination with a "success" string if the result
 		is bigger than or equal to the bias. Otherwise prints out the input 
@@ -52,4 +53,3 @@ class Perceptron:
 			print(f"Combination {input} returns True")
 		else:
 			print(f"Combination {input} returns False")
-
