2448091ec7e3f7cce01a63d8887439c955ce4317
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2022-02-05 20:28:42 +0100

    Updated main with PerceptronLayer + Network tests

--- Practicum1/Main.py
+++ Practicum1/Main.py
@@ -1,4 +1,6 @@
 from Perceptron import Perceptron
+from PerceptronLayer import PerceptronLayer
+from PerceptronNetwork import PerceptronNetwork
 
 combinations1 = [[0], [1]]
 combinations2 = [[0, 0], [0, 1], [1, 0], [1, 1]]
@@ -11,6 +13,7 @@ if __name__ == "__main__":
 	print("\n\n-----AND-GATE-----\n")
 	# Initialize the Perceptron
 	perceptron1 = Perceptron()
+
 	# Set the weights
 	perceptron1.set_weight([0.5, 0.5])
 	# Set the bias(threshold)
@@ -27,6 +30,7 @@ if __name__ == "__main__":
 	# OR-gate
 	print("\n\n-----OR-GATE-----\n")
 	perceptron2 = Perceptron()
+
 	perceptron2.set_weight([0.5, 0.5])
 	perceptron2.set_bias(0.5)
 
@@ -39,6 +43,7 @@ if __name__ == "__main__":
 	# INVERT-gate
 	print("\n\n-----INVERT-GATE-----\n")
 	perceptron3 = Perceptron()
+
 	perceptron3.set_weight([-1.0])
 	perceptron3.set_bias(-0.5)
 
@@ -51,6 +56,7 @@ if __name__ == "__main__":
 	# NOR-gate
 	print("\n\n-----NOR-GATE-----\n")
 	perceptron4 = Perceptron()
+
 	perceptron4.set_weight([-1.0, -1.0, -1.0])
 	perceptron4.set_bias(0)
 
@@ -63,9 +69,56 @@ if __name__ == "__main__":
 	# PARTY-PERCEPTRON
 	print("\n\n-----PARTY-PERCEPTRON-----\n")
 	perceptron5 = Perceptron()
+
 	perceptron5.set_weight([0.6, 0.3, 0.2])
 	perceptron5.set_bias(0.4)
 
 	for i in combinations3:
 		perceptron5.activate(i)	
 		perceptron5.__str__(i)
+
+
+
+	# Testing PerceptronLayer with AND-gate 
+	print("\n\n-----AND-GATE PerceptronLayer-----\n")
+	# Initialize the PerceptronLayer
+	perceptron_layer1 = PerceptronLayer()
+
+	# Initialize the Perceptrons
+	perceptron_layer1.create_perceptrons(2, 1)
+	# Set the weights
+	perceptron_layer1.set_weight([1, 1]) 
+	# Set the bias(threshold)
+	perceptron_layer1.set_bias([2])
+
+	for i in combinations2:
+		perceptron_layer1.activate(i)
+		perceptron_layer1.__str__(i)
+
+
+
+	# XOR-gate 
+	print("\n\n-----XOR-GATE-----\n")	
+	perceptron_network1 = PerceptronNetwork()
+
+	perceptron_network1.create_layers(2,[2],1)
+	perceptron_network1.set_weight([[[1, -1], [-1, 1]], [1, 1]])
+	perceptron_network1.set_bias([[1, 1], [1]])
+
+	for i in combinations2:
+		perceptron_network1.activate(i)
+		perceptron_network1.__str__(i)
+
+
+
+	# Half adder
+	print("\n\n-----Half adder-----\n")
+	perceptron_network1 = PerceptronNetwork()
+
+	perceptron_network1.create_layers(2,[2], 2)
+	perceptron_network1.set_weight([[[1, 1], [1, 1]], [[1], [-1, 1]]])
+	perceptron_network1.set_bias([[1, 2], [1, 1]])
+
+	for i in combinations2:
+		perceptron_network1.activate(i)
+		perceptron_network1.__str__(i)	
