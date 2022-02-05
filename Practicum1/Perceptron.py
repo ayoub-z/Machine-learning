2494713c3755d033391014d92593509a600c764e
ayoub-z <60653320+ayoub-z@users.noreply.github.com>
2022-02-05 13:30:12 +0100

    Updated Perceptron to work with PerceptronLayer

--- Practicum1/Perceptron.py
+++ Practicum1/Perceptron.py
@@ -2,7 +2,8 @@ class Perceptron:
 
 	def __init__(self):
 		self.weight = []
-		self.bias = None		
+		self.bias = None
+		self.sum = 0	
 
 	def set_weight(self, weights=list):
 		"""
@@ -20,21 +21,15 @@ class Perceptron:
 		"""
 		Activation function. 
 		Takes a binary input, such as [0, 1] and matches them with their corresponding 
-		weights (in order). The inputs coupled with their weights is then summarized. 
-		Finally the __str__ method is called with the result and input as the parameters.
+		weights (in order). The inputs coupled with their weights is then summarized.
 		"""
-		sum = 0
-		for counter, i in enumerate(input):
-			if i == 1:
-				sum += self.weight[counter] * i
-		return 1 if sum >= self.bias else 0
+		self.sum = 0
+		for counter, weight in enumerate(self.weight):
+			self.sum += input[counter] * weight
+		return 1 if self.sum - self.bias >= 0 else 0
 
 	def __str__(self, input):
 		"""
 		Prints the input combination and the output result.
 		"""
-
 		print(f"Input {input} returns: {self.activate(input)}")
-
-# inputs = [[0, 1, 1], [0, 1, 0], [0, 0, 0]]
-# weights = [0.2, 0.3, 0.4]
